0d9d625c9fdf02e4aef4f93d2181f10e498b6cef
BelaM80 <mueller.bela@gmx.de>
2020-10-28 12:00:30 +0100

    Create random Image with onclick

--- src/api/getRandomImage.js
+++ src/api/getRandomImage.js
@@ -2,8 +2,10 @@ export async function getRandomImage() {
   const url = "https://api.unsplash.com/photos/random";
   const response = await fetch(url, {
     method: "GET",
-    heeaders: {
+    headers: {
       Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
     },
   });
+  const randomImage = await response.json();
+  return randomImage;
 }
