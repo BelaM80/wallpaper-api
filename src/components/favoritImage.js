db196b61d71b3de05f382e2bcc94c0a1fdaff0b8
BelaM80 <mueller.bela@gmx.de>
2020-10-28 17:22:20 +0100

    Add photId

--- src/components/favoritImage.js
+++ src/components/favoritImage.js
@@ -1,12 +1,12 @@
 import "./favoritImage.css";
 import React from "react";
 
-export default function favoriteImage() {
+export default function favoriteImage({ photoId }) {
   return (
     <div className="favoriteCard">
       <img
         className="favoriteCard__img"
-        src="https://source.unsplash.com/UjD08DRW24E"
+        src={`https://source.unsplash.com/${photoId}`}
         alt="Something´s happening"
       />
     </div>
